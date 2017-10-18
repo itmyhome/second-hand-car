391e9998cc3594decda023a48a08a6948c4f698e
xie t <xietao@shicaidai.com>
2017-10-18 14:11:56 +0800

    新增api启动类，区分开发生产环境

---
+++ second-hand-car-api/src/main/java/com/secondhandcar/api/SecondHandCarApiApplication.java
@@ -0,0 +1,15 @@
+package com.secondhandcar.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by xiet on 2017/10/18.
+ */
+@SpringBootApplication
+public class SecondHandCarApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandCarApiApplication.class, args);
+    }
+}
